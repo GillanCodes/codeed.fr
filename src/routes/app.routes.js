31f4c2308f7b6b2d96ebc950081caaa035b0d1cd
gillancodes <gillancodes@laposte.net>
2022-04-04 17:42:48 +0200

    Opencodes.fr 70% done

--- src/routes/app.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-let router = require('express').Router();
-let appController = require("../controllers/app.controller.js");
-
-router.get('/', appController.index);
-
-module.exports = router;
